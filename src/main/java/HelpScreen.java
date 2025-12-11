111270d3de373ba194f259417976579af4c23d65
ajiteshsn <ajiteshsankara@gmail.com>
2025-12-11 16:53:42 -0500

    Help screen

--- src/main/java/HelpScreen.java
+++ src/main/java/HelpScreen.java
@@ -1,11 +1,61 @@
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.geometry.Pos;
 
 public class HelpScreen {
     
+    private Stage stage;
+    
+    public HelpScreen(Stage stage) {
+        this.stage = stage;
+    }
+    
+    public void show() {
+        // Create title
+        Label titleLabel = new Label("How to Play Connect 4");
+        titleLabel.setFont(new Font("Arial", 28));
+        titleLabel.setStyle("-fx-font-weight: bold;");
+        
+        // Create instructions
+        Label instructions1 = new Label("1. Two players take turns: Red and Yellow");
+        instructions1.setFont(new Font("Arial", 16));
+        
+        Label instructions2 = new Label("2. Press keys 1-7 to drop your coin in that column");
+        instructions2.setFont(new Font("Arial", 16));
+        
+        Label instructions3 = new Label("3. Coins stack on top of each other");
+        instructions3.setFont(new Font("Arial", 16));
+        
+        Label instructions4 = new Label("4. Get 4 coins in a row to WIN!");
+        instructions4.setFont(new Font("Arial", 16));
+        
+        Label instructions5 = new Label("5. You can win horizontally, vertically, or diagonally");
+        instructions5.setFont(new Font("Arial", 16));
+        
+        // Create back button
+        Button backButton = new Button("Back to Menu");
+        backButton.setFont(new Font("Arial", 18));
+        backButton.setPrefSize(150, 40);
+        
+        // When back button is clicked, go back to start screen
+        backButton.setOnAction(e -> {
+            StartScreen startScreen = new StartScreen(stage);
+            startScreen.show();
+        });
+        
+        // Create layout
+        VBox layout = new VBox(15);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(titleLabel, instructions1, instructions2, 
+                                     instructions3, instructions4, instructions5, backButton);
+        layout.setStyle("-fx-background-color: #87CEEB;");
+        
+        // Create scene
+        Scene scene = new Scene(layout, 700, 600);
+        stage.setScene(scene);
+    }
 }
--- src/main/java/StartScreen.java
+++ src/main/java/StartScreen.java
@@ -1,10 +1,10 @@
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.geometry.Pos;
 
 public class StartScreen {
     
@@ -17,15 +17,15 @@ public class StartScreen {
     public void show() {
         // Create welcome message
         Label welcomeLabel = new Label("Welcome to Connect 4!");
-        welcomeLabel.setFont(new Font("Courier New", 32));
+        welcomeLabel.setFont(new Font("Arial", 32));
         
         // Create instructions
         Label instructionsLabel = new Label("Press keys 1-7 to drop your coin");
-        instructionsLabel.setFont(new Font("Courier New", 16));
+        instructionsLabel.setFont(new Font("Arial", 16));
         
         // Create start button
         Button startButton = new Button("Start Game");
-        startButton.setFont(new Font("Courier New", 20));
+        startButton.setFont(new Font("Arial", 20));
         startButton.setPrefSize(150, 50);
         
         // When start button is clicked, show the game screen
@@ -34,11 +34,22 @@ public class StartScreen {
             gameScreen.show();
         });
         
+        // Create help button
+        Button helpButton = new Button("How to Play");
+        helpButton.setFont(new Font("Arial", 20));
+        helpButton.setPrefSize(150, 50);
+        
+        // When help button is clicked, show the help screen
+        helpButton.setOnAction(e -> {
+            HelpScreen helpScreen = new HelpScreen(stage);
+            helpScreen.show();
+        });
+        
         // Create layout
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(welcomeLabel, instructionsLabel, startButton);
-        layout.setStyle("-fx-background-color: #84c5e0;");
+        layout.getChildren().addAll(welcomeLabel, instructionsLabel, startButton, helpButton);
+        layout.setStyle("-fx-background-color: #87CEEB;");
         
         // Create scene
         Scene scene = new Scene(layout, 700, 600);

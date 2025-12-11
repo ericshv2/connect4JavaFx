17cc85ab087729199db9cb8a60e7079bd6ed6936
ericshv2 <134001817+ericshv2@users.noreply.github.com>
2025-12-11 14:21:14 -0500

    Help Screen Start + Audio

---
+++ src/main/java/HelpScreen.java
@@ -0,0 +1,11 @@
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
+public class HelpScreen {
+    
+}
